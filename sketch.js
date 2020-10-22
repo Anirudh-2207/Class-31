a14f2cc3f1b7aa5b0e6f43aa86463987ffc49722
Anirudh <gamotricks@gmail.com>
2020-10-22 16:47:07 +0530

    added gameStates

--- sketch.js
+++ sketch.js
@@ -1,3 +1,34 @@
+var string = "this is string";
+console.log(string);
+
+var num = 100;
+console.log(num);
+
+var bool = true;
+console.log(bool)
+
+
+var object;
+console.log(object);
+
+object = null;
+console.log(null);
+
+var arra1 = [1,2,3,4,5];
+console.log(arra1);
+
+var arra2 = [1, "name", true, null];
+console.log(arra2)
+
+var arra3 = [[1,2],[2,3],[3,4]]
+
+arra3.push("my name")
+console.log(arra3)
+
+
+
+
+
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
@@ -7,7 +38,7 @@ var engine, world;
 var box1, pig1,pig3;
 var backgroundImg,platform;
 var bird, slingshot;
-
+var gameState = "onSling"
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -69,16 +100,18 @@ function draw(){
 }
 
 function mouseDragged(){
+    if(gameState!== "launched") {
     Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
 }
-
+}
 
 function mouseReleased(){
     slingshot.fly();
+    gameState = "launched"
 }
 
 function keyPressed(){
     if(keyCode === 32){
-        slingshot.attach(bird.body);
+       // slingshot.attach(bird.body);
     }
 }
